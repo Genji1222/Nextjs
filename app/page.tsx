49defe90a77f9998ef8998cd846992670913b311
Your Name <you@example.com>
2024-01-29 22:24:05 -0800

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+
 export default function Page() {
   return (
     <>
-      <div className="w-[200px] h-[50px] text-blue-500 ml-2 mt-5">tailwindCSS</div>
-      <div className={styles.shape}/>
+      <div className="ml-2 mt-5 h-[50px] w-[200px] text-blue-500">
+        tailwindCSS
+      </div>
+      <div className={styles.shape} />
+      <div className="flex items-center justify-center p-6 py-12 md:w-3/5 md:px-28">
+      <img
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      </div>
     </>
   );
 }
